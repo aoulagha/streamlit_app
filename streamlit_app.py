609ded57d0cf131cff13b383b0a71cf32be5ed6c
oulagha <oulaghaamine@gmail.com>
2024-09-13 13:33:12 +0100

    add : ajout premier onglet

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ import numpy as np
 
 st.title("Formation Python")
 
-tab1, tab2, tab3 = st.tabs(["Présentation de l'écosystème", "La SciPy Stack", "Librairies de visualisation", "Fichiers volumineux"])
+tab1, tab2, tab3, tab4 = st.tabs(["Présentation de l'écosystème", "La SciPy Stack", "Librairies de visualisation", "Fichiers volumineux"])
 
 with tab1:
-    tab4, tab5, tab6 = st.tabs(["Guide", "Quiz sur venv", "Voir les réponses"])
-    
+    tab11, = st.tabs(["Guide"])
+
+    with tab4:
+        st.title("Guide de Gestion des Environnements Python avec venv")
